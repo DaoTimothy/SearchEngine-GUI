699775a201e7527751936ddfdbeb2ba8f41eeaa9
DaoTimothy <79426229+DaoTimothy@users.noreply.github.com>
2022-12-08 14:14:42 -0500

    finished gui

--- src/SearchAppView.java
+++ src/SearchAppView.java
@@ -1,6 +1,6 @@
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
-import javafx.scene.control.ListView;
+import javafx.scene.shape.Circle;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,26 +8,50 @@ import java.util.List;
 public class SearchAppView extends Pane {
     private ListView<String> resultTitles;
     private ListView<Double> resultScores;
+    private TextField searchBox;
+    private RadioButton pageRankToggle;
+
+    public RadioButton getPageRankToggle() {
+        return pageRankToggle;
+    }
+
+    public TextField getSearchBox() {
+        return searchBox;
+    }
 
     public SearchAppView() {
-        Label label1 = new Label("Title");
-        label1.relocate(10, 10);
-        Label label2 = new Label("Score");
-        label2.relocate(220, 10);
+        Label searchLabel = new Label("Search");
+        Label pageRankLabel = new Label("Include PageRank");
+        Label titleLabel = new Label("Result Titles");
+        Label scoreLabel = new Label("Result Scores");
+
+        searchBox = new TextField("");
+        searchBox.setPromptText("Enter your query");
+        searchBox.relocate(10, 30);
+        searchBox.setPrefSize(410, 25);
+        searchLabel.relocate(10,10);
+
+        pageRankToggle = new RadioButton();
+        pageRankToggle.relocate(10, 55);
+        pageRankToggle.setPrefSize(50, 25);
+        pageRankLabel.relocate(40, 55);
+        pageRankLabel.setPrefSize(100, 25);
 
         resultTitles = new ListView<String>();
-        resultTitles.relocate(10, 40);
-        resultTitles.setPrefSize(200,150);
+        resultTitles.relocate(10, 110);
+        resultTitles.setPrefSize(200,235);
+        titleLabel.relocate(10, 90);
 
         resultScores = new ListView<Double>();
-        resultScores.relocate(220, 40);
-        resultScores.setPrefSize(60,150);
+        resultScores.relocate(220, 110);
+        resultScores.setPrefSize(200,235);
+        scoreLabel.relocate(220,90);
 
 
         // Add all the components to the Pane
-        getChildren().addAll(resultScores, resultTitles);
+        getChildren().addAll(searchBox, searchLabel, pageRankToggle, pageRankLabel, resultTitles, titleLabel, resultScores, scoreLabel);
 
-        setPrefSize(360, 240);
+        setPrefSize(430, 355);
     }
 
     public void update(Project model, String query, boolean boost, int n) {
